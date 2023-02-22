9f3c9bd1bdcc6854ee9e57e378452cb571d7fd6f
salasberryfin <salash66@gmail.com>
2023-02-22 20:28:56 +0100

    add standard DNS proxy functionality

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ DNS proxy:
 
 Features:
 	- Listen for TCP requests: concurrently
-	- Parse connection message
 	- Establish TLS connection to DNS-over-TLS end
 		- Certificate
 	- Receive TCP response
@@ -17,54 +16,98 @@ Extra:
 package main
 
 import (
+	"crypto/tls"
 	"io"
 	"log"
 	"net"
-	"strings"
 )
 
 const (
-	protocol = "tcp"
-	port     = "5353" // running on 5353 to avoid having to use sudo during development
+	protocol         = "tcp"     // protocol the proxy listens to defaults to tcp
+	port             = "5353"    // running on 5353 to avoid having to use sudo during development
+	dnsServerHost    = "1.1.1.1" // use Cloudflare DNS server
+	dnsServerPort    = "53"
+	dnsServerPortTLS = "853"
 )
 
-func parseDNSMessage(buf []byte) {
-	/*
-		DNS request format:
-			- 2: Trans ID
-			- 2: Parameters
-			- byte 14: start of the `queries` field - non-fixed length
-	*/
-	dnsName := []string{}
-	queryFirstByte := 14
-	length := 0
-	for {
-		current := length + queryFirstByte
-		if buf[current] == 0 {
-			break
-		}
-		dnsName = append(dnsName, string(buf[queryFirstByte:buf[current]+byte(queryFirstByte)+1]))
-		queryFirstByte += int(buf[current]) + 1
+// parseResponse reads the data received on the connection
+func parseResponse(conn net.Conn) ([]byte, error) {
+	buf := make([]byte, 0, 4096)
+	text := make([]byte, 256)
+	n, err := conn.Read(text)
+	if err != nil && err != io.EOF {
+		return nil, err
 	}
+	buf = append(buf, text[:n]...)
 
-	log.Printf("Domain: %s\n", strings.Join(dnsName, "."))
+	return buf, nil
 }
 
-func handleRequest(conn net.Conn) {
+// resolve gets the request content and forwards it to the end DNS server
+func resolveTLS(buf []byte) ([]byte, error) {
+	addr := dnsServerHost + ":" + dnsServerPortTLS
+	log.Printf("Establishing TLS connection with %s\n", addr)
+	conf := &tls.Config{}
+	conn, err := tls.Dial("tcp", addr, conf)
+	if err != nil {
+		return nil, err
+	}
+	log.Printf("Connection established\n")
+	_, err = conn.Write(buf)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := parseResponse(conn)
+	if err != nil {
+		return nil, err
+	}
+	conn.Close()
+
+	return resp, nil
+}
+
+// resolve gets the request content and forwards it to the end DNS server
+func resolve(buf []byte) ([]byte, error) {
+	addr := dnsServerHost + ":" + dnsServerPort
+	log.Printf("Connecting to DNS server %s\n", addr)
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		return nil, err
+	}
+	_, err = conn.Write(buf)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := parseResponse(conn)
+	if err != nil {
+		return nil, err
+	}
+	conn.Close()
+
+	return resp, nil
+}
+
+// handler processed the dns requests received from the clients
+func handler(conn net.Conn) {
 	// check with `kdig -d @localhost -p 5353 +tcp example.com`
 	buf := make([]byte, 0, 4096)
 	text := make([]byte, 256)
 	n, err := conn.Read(text)
-	if err != nil {
-		if err != io.EOF {
-			log.Fatalf("Erorr reading input: %v\n", err)
-		}
+	if err != nil && err != io.EOF {
+		log.Fatalf("Erorr reading input: %v\n", err)
 	}
 	buf = append(buf, text[:n]...)
-	parseDNSMessage(buf)
+	//resp, err := resolve(buf)
+	resp, err := resolveTLS(buf)
+	if err != nil {
+		log.Fatalf("Failed to request domain resolution: %s\n", err)
+	}
+	conn.Write(resp)
+
 	conn.Close()
 }
 
+// main simply creates a new TCP server to act as proxy
 func main() {
 	ln, err := net.Listen(protocol, ":"+port)
 	if err != nil {
@@ -78,6 +121,6 @@ func main() {
 			log.Print(err)
 			continue
 		}
-		go handleRequest(conn)
+		go handler(conn)
 	}
 }
