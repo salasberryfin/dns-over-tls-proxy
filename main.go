4f81bd1e633a4892520f298ee6f0d3d6d459aa5e
salasberryfin <salash66@gmail.com>
2023-02-23 18:01:48 +0100

    proxy is now functional for TCP requests

--- main.go
+++ main.go
@@ -17,23 +17,25 @@ package main
 
 import (
 	"crypto/tls"
+	"fmt"
 	"io"
 	"log"
 	"net"
+	"time"
 )
 
 const (
-	protocol         = "tcp"     // protocol the proxy listens to defaults to tcp
-	port             = "5353"    // running on 5353 to avoid having to use sudo during development
-	dnsServerHost    = "1.1.1.1" // use Cloudflare DNS server
-	dnsServerPort    = "53"
-	dnsServerPortTLS = "853"
+	protocol          = "tcp"                // protocol the proxy listens to defaults to tcp
+	port              = "5353"               // running on 5353 to avoid having to use sudo during development
+	dnsServerHost     = "1.1.1.1"            // use Cloudflare DNS server
+	dnsServerHostName = "cloudflare-dns.com" // hostname to validate the certificate against
+	dnsServerPortTLS  = "853"                // DNS over TLS port
 )
 
-// parseResponse reads the data received on the connection
-func parseResponse(conn net.Conn) ([]byte, error) {
+// parseMessage reads the data received on the connection
+func parseMessage(conn net.Conn) ([]byte, error) {
 	buf := make([]byte, 0, 4096)
-	text := make([]byte, 256)
+	text := make([]byte, 1024)
 	n, err := conn.Read(text)
 	if err != nil && err != io.EOF {
 		return nil, err
@@ -43,7 +45,24 @@ func parseResponse(conn net.Conn) ([]byte, error) {
 	return buf, nil
 }
 
-// resolve gets the request content and forwards it to the end DNS server
+// validateCert checks that the certificate is valid and has not expired
+func validateCert(conn *tls.Conn) error {
+	// verify server certificate
+	log.Printf("Validating certificate for %s\n", dnsServerHostName)
+	err := conn.VerifyHostname(dnsServerHostName)
+	if err != nil {
+		return err
+	}
+	expires := conn.ConnectionState().PeerCertificates[0].NotAfter
+	diff := expires.Sub(time.Now())
+	if diff <= 0 {
+		return fmt.Errorf("The certitificate expired on %v\n", expires)
+	}
+
+	return nil
+}
+
+// resolveTLS gets the request content and forwards it to the end DNS server over TLS
 func resolveTLS(buf []byte) ([]byte, error) {
 	addr := dnsServerHost + ":" + dnsServerPortTLS
 	log.Printf("Establishing TLS connection with %s\n", addr)
@@ -52,59 +71,40 @@ func resolveTLS(buf []byte) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-	log.Printf("Connection established\n")
+	defer conn.Close()
+	err = validateCert(conn)
+	if err != nil {
+		return nil, err
+	}
+	log.Printf("Connection established!\n")
 	_, err = conn.Write(buf)
 	if err != nil {
 		return nil, err
 	}
-	resp, err := parseResponse(conn)
+	resp, err := parseMessage(conn)
 	if err != nil {
 		return nil, err
 	}
-	conn.Close()
 
 	return resp, nil
 }
 
-// resolve gets the request content and forwards it to the end DNS server
-func resolve(buf []byte) ([]byte, error) {
-	addr := dnsServerHost + ":" + dnsServerPort
-	log.Printf("Connecting to DNS server %s\n", addr)
-	conn, err := net.Dial("tcp", addr)
-	if err != nil {
-		return nil, err
-	}
-	_, err = conn.Write(buf)
-	if err != nil {
-		return nil, err
-	}
-	resp, err := parseResponse(conn)
-	if err != nil {
-		return nil, err
-	}
-	conn.Close()
-
-	return resp, nil
-}
-
-// handler processed the dns requests received from the clients
+// handler processes the DNS requests received from the client
 func handler(conn net.Conn) {
 	// check with `kdig -d @localhost -p 5353 +tcp example.com`
-	buf := make([]byte, 0, 4096)
-	text := make([]byte, 256)
-	n, err := conn.Read(text)
-	if err != nil && err != io.EOF {
-		log.Fatalf("Erorr reading input: %v\n", err)
+	buf, err := parseMessage(conn)
+	if err != nil {
+		log.Printf("Failed to format input: %v", err)
+		return
 	}
-	buf = append(buf, text[:n]...)
-	//resp, err := resolve(buf)
+	defer conn.Close()
 	resp, err := resolveTLS(buf)
 	if err != nil {
-		log.Fatalf("Failed to request domain resolution: %s\n", err)
+		log.Printf("Failed to request domain resolution: %s\n", err)
+		return
 	}
 	conn.Write(resp)
-
-	conn.Close()
+	log.Printf("Server response sent\n")
 }
 
 // main simply creates a new TCP server to act as proxy
